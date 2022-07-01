5df085634cdd2d618d9b62f9d809ee77c0fdb009
Joy-pearl2 <omohwovoapeweoghene@gmail.com>
2022-06-30 23:29:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-gcc #include <stdio.h>
+#include <stdio.h>
 /
  * main - Entry point
  *  * Return: Always 0 (sucess)
@@ -7,5 +6,6 @@ gcc #include <stdio.h>
  int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);  
-}  
+	return (0);
+}
+  
