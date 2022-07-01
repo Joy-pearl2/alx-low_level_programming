0cd14fb899ed053c7c47385d96b77d9a2725beac
Joy-pearl2 <omohwovoapeweoghene@gmail.com>
2022-07-01 09:12:48 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
- 
 /**
- *  * main - Entry point
+ *  * main - Prints ""programming is like building a multilingual puzzle,"
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: 0
  *     */
  
 int main(void)
@@ -12,5 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	 
 	return (0);
-}
- 
+} 
