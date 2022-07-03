b1650d36e9e591105ebdd3b2576db9ea63949d43
Joy-pearl2 <omohwovoapeweoghene@gmail.com>
2022-07-03 08:13:20 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+
+#include <studio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Prints sizes of various types on the computer complied and run on
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
