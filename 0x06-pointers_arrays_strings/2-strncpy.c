1910531398074ad05285cdcea118d3824dd2acfc
Joy-pearl2 <omohwovoapeweoghene@gmail.com>
2022-07-15 11:33:45 -0700

    strcmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,22 +5,23 @@
  * of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
- * @n: The maximum number of bytes to copied from src. 
+ * @n: The maximum number of bytes to copied from src
  *
- * Return: A pointer to the resulting string dest. 
+ * Return: A pointer to the resulting string dest
  */
-char *_strncpy(char *dest, char *src, int n)
+
+char * _strncpy(char * dest, char * src, int n)
 {
-	int index = 0, src_len = 0;
+	int i;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] &&index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; indec < n; index++)
-		dest[index] = '\0';
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
 
 	return (dest);
 }
